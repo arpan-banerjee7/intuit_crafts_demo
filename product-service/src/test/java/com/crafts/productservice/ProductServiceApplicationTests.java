8ff85c31687b794507516702d35e586848980234
Arpan <arpan.nbanerjee7@gmail.com>
2023-10-18 21:54:13 +0530

    initial commit

---
+++ product-service/src/test/java/com/crafts/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crafts.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
